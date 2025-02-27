fa6e7b72e3431b0b289d7d07869a660bcf8f6155
Saeed-L5 <9cksb6xw@students.codeinstitute.net>
2025-02-27 14:53:08 +0000

    Create board style using Css and start type some Jquery JS

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    let currentplayer = 'X';
+    let game = ['', '', '', '', '', '', '', '', ''];
+    let gameActive = true;
+    let scores = {X:0, O:0, D: 0};
+
+    // Create game board 
+for (let i = 0; i < 9; i++) {
+    $('#board').append('<div class="cell data-index="' + i + '"></div>')
+    
+}
+})
