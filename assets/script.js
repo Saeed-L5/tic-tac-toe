bf6548796efa79cf6c27d75fc186f6ad59e7111e
Saeed-L5 <9cksb6xw@students.codeinstitute.net>
2025-03-02 21:04:47 +0000

    Create checkWin in JS using Jquery and conditions

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,48 @@
-$(document).ready(function() {
+$(document).ready(function () {
     let currentplayer = 'X';
     let game = ['', '', '', '', '', '', '', '', ''];
     let gameActive = true;
-    let scores = {X:0, O:0, D: 0};
+    let scores = { X: 0, O: 0, D: 0 };
 
     // Create game board 
     for (let i = 0; i < 9; i++) {
         $('#board').append('<div class="cell" data-index="' + i + '"></div>');
     }
 
-// Create Cell Click 
-$('.cell').click(function() {
-    let index = $(this).data('index');
+    // Create Cell Click 
+    $('.cell').click(function () {
+        let index = $(this).data('index');
 
-    if (game[index] === '' && gameActive) {
-        game[index] = currentplayer;
-        $(this).addClass(currentplayer === 'X' ? 'x-mark' : 'o-mark').text(currentplayer);
+        if (game[index] === '' && gameActive) {
+            game[index] = currentplayer;
+            $(this).addClass(currentplayer === 'X' ? 'x-mark' : 'o-mark').text(currentplayer);
+
+            if (checkWin()) {
+                handleWin();
+            }
+        }
+
+    })
+
+    // Check win conditions
+    function checkWin() {
+        const winPatterns = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], //Rows
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], //Columns
+            [0, 4, 8], [2, 4, 6], // Diagonals
+        ];
+
+        return winPatterns.some(pattern => {
+            if (pattern.every(index => game[index] === currentplayer)) {
+                pattern.forEach(i => {
+                    return $(`.cell[data-index="${i}"]`).css('transform', 'scale(1.1)');
+                });
+                return true;
+            }
+            return false;
+
+        })
+
+    
     }
-
-})
 })
